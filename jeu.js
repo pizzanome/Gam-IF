e5c9d9f7d95f1c64dd6fda8a8ca65017c0166866
Juan Valero <98854476+juanvlr@users.noreply.github.com>
2022-12-29 14:42:28 +0100

    Oublié de supprimer un commentaire (oups)

--- jeu.js
+++ jeu.js
@@ -23,39 +23,6 @@ function getData() {
 }
 
 function printData(data) {
-    /*
-    PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>PREFIX dc: <http://purl.org/dc/elements/1.1/>
-        PREFIX : <http://dbpedia.org/resource/>
-        PREFIX dbpedia2: <http://dbpedia.org/property/>
-        PREFIX dbpedia: <http://dbpedia.org/>
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        SELECT *
-        WHERE {
-        <http://dbpedia.org/resource/Nintendo_Switch> rdfs:label ?name.
-        FILTER(langMatches(lang(?name),"FR"))
-        }
-     */
-
-    /*
-     PREFIX owl: <http://www.w3.org/2002/07/owl#>
-        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-        PREFIX foaf: <http://xmlns.com/foaf/0.1/>PREFIX dc: <http://purl.org/dc/elements/1.1/>
-        PREFIX : <http://dbpedia.org/resource/>PREFIX dbpedia2: <http://dbpedia.org/property/>
-        PREFIX dbpedia: <http://dbpedia.org/>
-        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        SELECT ?name ?dev ?directeur ?publisher ?description (GROUP_CONCAT(DISTINCT ?dates;SEPARATOR=";") AS ?date) (GROUP_CONCAT(DISTINCT ?genres;SEPARATOR=";") AS ?genre) (GROUP_CONCAT(DISTINCT ?plateformes;SEPARATOR=";") AS ?plateforme)
-        WHERE {
-        <http://dbpedia.org/resource/Mario's_Game_Gallery> foaf:name ?name; dbp:genre ?genres; dbo:releaseDate ?dates; dbp:developer ?dev; dbo:publisher ?publisher; dbo:abstract ?description; dbp:platforms ?plateformes.
-        FILTER(langMatches(lang(?description),"FR"))
-        OPTIONAL{<http://dbpedia.org/resource/Mario's_Game_Gallery> dbp:director ?directeur}
-        }
-     */
     document.getElementById("jeu-nom").innerHTML = data.results.bindings[0].name.value;
 
     getImageFromWikipedia(data.results.bindings[0].name.value)
