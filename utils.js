0e3ba9aafa8c4ede72843909175835254f8076b8
Kamin0 <nathanpierret19@gmail.com>
2022-12-30 13:47:22 +0100

    Mise en commentaire/suppression des méthodes inutilisés

--- utils.js
+++ utils.js
@@ -17,7 +17,7 @@ function executeSparqlRequest(request) {
     });
 }
 
-function getImageFromWikipedia(game) {
+/*function getImageFromWikipedia(game) {
     const gameNorm = encodeURIComponent(game);
     const url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&titles=${gameNorm}&prop=images&format=json`;
 
@@ -42,7 +42,7 @@ function getImageFromWikipedia(game) {
                     return (image !== undefined && image.length > 0) ? image[0].url : "";
                 });
         });
-}
+}*/
 
 function getImageGBApi(nom) {
     const key = "361817f45f87302548f18c9121d15e9d227db4af";
@@ -55,30 +55,4 @@ function getImageGBApi(nom) {
         dataType: "jsonp",
         jsonp: "json_callback"
     });
-}
-
-function getImageFromWikidata(game) {
-    const gameNorm = encodeURIComponent(game);
-    const url = `https://www.wikidata.org/w/api.php?action=wbgetentities&format=json&sites=enwiki&props=claims&titles=${gameNorm}`;
-
-    return fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const entities = data.entities;
-            const entity = entities[Object.keys(entities)[0]];
-            const claims = entity.claims;
-            const claim = claims.P18[0];
-            const image = claim.mainsnak.datavalue.value;
-
-            const imageUrl = `https://commons.wikimedia.org/wiki/Special:FilePath/${image}`;
-            return fetch(imageUrl)
-                .then(response => response.json())
-                .then(data => {
-                    const pages = data.query.pages;
-                    const page = pages[Object.keys(pages)[0]];
-                    const image = page.imageinfo;
-
-                    return (image !== undefined && image.length > 0) ? image[0].url : "";
-                });
-        });
 }
