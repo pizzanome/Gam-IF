74f300c118010e7e40c8d67d50fa3848962a56fb
Kamin0 <nathanpierret19@gmail.com>
2022-12-30 18:18:05 +0100

    Recopie méthode suite au merge

--- utils.js
+++ utils.js
@@ -42,7 +42,7 @@ function executeSparqlRequest(request) {
                     return (image !== undefined && image.length > 0) ? image[0].url : "";
                 });
         });
-}
+}*/
 
 function getImageGBApi(nom) {
     const key = "361817f45f87302548f18c9121d15e9d227db4af";
@@ -55,4 +55,61 @@ function getImageGBApi(nom) {
         dataType: "jsonp",
         jsonp: "json_callback"
     });
+}
+
+function printResourceName(ressource, id) {
+    const request = `
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>PREFIX dc: <http://purl.org/dc/elements/1.1/>
+        PREFIX : <http://dbpedia.org/resource/>
+        PREFIX dbpedia2: <http://dbpedia.org/property/>
+        PREFIX dbpedia: <http://dbpedia.org/>
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        SELECT *
+        WHERE {
+        <${ressource}> rdfs:label ?name.
+        FILTER(langMatches(lang(?name),"FR"))
+        }`;
+
+    executeSparqlRequest(request)
+        .then(data => {if(data.results.bindings[0] !== undefined){
+            document.getElementById(id).innerHTML += `<span class="badge bg-primary badge-pill">${data.results.bindings[0].name.value}</span>`;
+        }else{
+            if(ressource.includes("http://dbpedia.org/resource/")){
+                document.getElementById(id).innerHTML += `<span class="badge bg-primary badge-pill">${ressource.split("http://dbpedia.org/resource/")[1]}</span>`;
+            }else {
+                document.getElementById(id).innerHTML += `<span class="badge bg-primary badge-pill">${ressource}</span>`;
+            }
+        }});
+}
+
+function printPlateformeLink(ressource, id) {
+    if(ressource.includes("http://dbpedia.org/resource/")){
+        document.getElementById(id).innerHTML += `<a href="plateforme.html?ressource=${ressource}" class="badge bg-primary badge-pill">${ressource.split("http://dbpedia.org/resource/")[1]}</a>`;
+    }else {
+        const request = `
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>PREFIX dc: <http://purl.org/dc/elements/1.1/>
+        PREFIX : <http://dbpedia.org/resource/>
+        PREFIX dbpedia2: <http://dbpedia.org/property/>
+        PREFIX dbpedia: <http://dbpedia.org/>
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        SELECT ?plateforme
+        WHERE {
+        ?plateforme a dbo:VideoGame; a dbo:Device; rdfs:label ?name.
+        FILTER(regex(?name,"${ressource}"))
+        }`;
+
+        executeSparqlRequest(request)
+            .then(data => {
+                var plateforme = data.results.bindings[0].plateforme.value;
+                document.getElementById(id).innerHTML += `<a href="plateforme.html?ressource=${plateforme}" class="badge bg-primary badge-pill">${ressource}</a>`;
+            });
+    }
 }
