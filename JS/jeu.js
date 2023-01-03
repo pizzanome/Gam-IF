5b0b99f0658764a604ed3f14c241da0959cf7e96
François MOURIER <francois.mourier@gmail.com>
2023-01-03 20:01:49 +0100

    Avancé mais non fonctionnel

--- JS/jeu.js
+++ JS/jeu.js
@@ -11,11 +11,13 @@ function getData() {
         PREFIX : <http://dbpedia.org/resource/>PREFIX dbpedia2: <http://dbpedia.org/property/>
         PREFIX dbpedia: <http://dbpedia.org/>
         PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-        SELECT ?name ?dev ?directeur ?publisher ?description (GROUP_CONCAT(DISTINCT ?dates;SEPARATOR=";") AS ?date) (GROUP_CONCAT(DISTINCT ?genres;SEPARATOR=";") AS ?genre) (GROUP_CONCAT(DISTINCT ?plateformes;SEPARATOR=";") AS ?plateforme)
+        SELECT ?name ?dev ?directeur ?publisher ?description (GROUP_CONCAT(DISTINCT ?dates;SEPARATOR=";") AS ?date) (GROUP_CONCAT(DISTINCT ?genres;SEPARATOR=";") AS ?genre) (GROUP_CONCAT(DISTINCT ?plateformes;SEPARATOR=";") AS ?plateforme) ?serie (GROUP_CONCAT(DISTINCT ?jeu;SEPARATOR=";") AS ?jeux)
         WHERE {
-        <${ressource}> rdfs:label ?name; dbp:genre ?genres; dbo:releaseDate ?dates; dbp:developer ?dev; dbo:publisher ?publisher; dbo:abstract ?description; dbp:platforms ?plateformes.
+        VALUES ?ressource {<${ressource}>}
+        ?ressource rdfs:label ?name; dbp:genre ?genres; dbo:releaseDate ?dates; dbp:developer ?dev; dbo:publisher ?publisher; dbo:abstract ?description; dbp:platforms ?plateformes.
         FILTER(langMatches(lang(?description),"FR") && langMatches(lang(?name),"FR"))
-        OPTIONAL{<${ressource}> dbp:director ?directeur}
+        OPTIONAL{?ressource dbp:director ?directeur}
+        OPTIONAL{?ressource dbo:series ?serie. ?serie dbp:game ?jeu. FILTER(?jeu != ?ressource)}
         }`;
 
     executeSparqlRequest(request)
@@ -51,8 +53,43 @@ function printData(data) {
 
     document.getElementById("jeu-description").innerHTML = data.results.bindings[0].description.value;
 
+    if(data.results.bindings[0].serie !== undefined) {
+        document.getElementById("jeu-serie").innerHTML = "Jeux de la série " + data.results.bindings[0].serie.value + " : <br><ul>";
+        const jeux = data.results.bindings[0].jeux.value.split(";");
+        for (let i = 0; i < jeux.length; i++) {
+            document.getElementById("jeu-serie").innerHTML += `<li>`;
+            printListGame(jeux[i], "jeu-serie");
+            document.getElementById("jeu-serie").innerHTML += `</li>`;
+        }
+        document.getElementById("jeu-serie").innerHTML += "</ul>";
+    }
+
     const plateformes = data.results.bindings[0].plateforme.value.split(";");
     for (let i = 0; i < plateformes.length; i++) {
         printPlateformeLink(plateformes[i], "jeu-plateforme");
     }
+}
+
+function printListGame(ressource, id){
+    const request = `
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX foaf: <http://xmlns.com/foaf/0.1/>PREFIX dc: <http://purl.org/dc/elements/1.1/>
+    PREFIX : <http://dbpedia.org/resource/>
+    PREFIX dbpedia2: <http://dbpedia.org/property/>
+    PREFIX dbpedia: <http://dbpedia.org/>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+    SELECT ?name
+    WHERE {
+    <${ressource}> rdfs:label ?name.
+    FILTER(langMatches(lang(?name),"FR"))
+    }`;
+
+    executeSparqlRequest(request)
+        .then(data => {
+            var jeu = data.results.bindings[0].name.value;
+            document.getElementById(id).innerHTML += `<a href="plateforme.html?ressource=${ressource}">${jeu}</a><br>`;
+        });
 }
